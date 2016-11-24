305ed0be8236270b4bc237bbbd4fb325aed4566d
dance2die <dontbothermewithspam@gmail.com>
2016-11-24 15:45:30 -0500

    Created ConsoleApplication1 to rebut someone on DevRant

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringClass sc = new StringClass();
+            sc.setField("new value");
+
+            Console.WriteLine(sc.getField());
+        }
+    }
+
+    internal class StringClass
+    {
+        private string field = "default";
+
+        public void setField(string field)
+        {
+            field = field;
+        }
+
+        public string getField()
+        {
+            return field;
+        }
+    }
+}
